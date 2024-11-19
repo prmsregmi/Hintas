cb38c9c19cc87127b24ea8eca48d30961298da60
Pramesh <prms.regmi@gmail.com>
2024-11-18 20:47:05 -0500

    gitignore

--- hintas/settings.py
+++ hintas/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-66gjd^dqd6o5+-2nyq!)%^-=(d!1rtg@f!lhu07gu@2hj=o(39"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'hintas.com', 'letsfutsal.com', 'www.letsfutsal.com', '64.227.140.63', 'www.hintas.com', 'sorters.ulpu.com.np', 'www.sorters.ulpu.com.np']
 
